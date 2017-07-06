e2898250016b79da2cba5209ca99ad91daf8c8aa
beebeean09 <beebeean09@gmail.com>
2017-07-05 22:19:36 -0700

    add navbar to integrate

---
+++ navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, NavigatorIOS } from 'react-native';
+import App from './App';
+
+class Navbar extends React.Component {
+  render() {
+
+    return(
+      <NavigatorIOS
+        initialRoute={{
+          component: App,
+          title: 'Home Page'
+        }}
+        style={{ flex: 1 }}
+      />
+    );
+  }
+}
+
+export default Navbar;
