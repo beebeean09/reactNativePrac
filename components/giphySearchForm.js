2df23c702b92c0258e2521b2e2389e6febcffb0f
beebeean09 <beebeean09@gmail.com>
2017-07-07 21:11:27 -0700

    made api call to giphy for searchterm

--- components/giphySearchForm.js
+++ components/giphySearchForm.js
@@ -6,8 +6,42 @@ class GiphySearchForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { first: "", last: "", email: ""};
+    // this.state = { searchTerm: "", giphys: ""};
+    this.state = { first: "", last: "", email: "", searchTerm: "", giphy: ""};
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.alertInput = this.alertInput.bind(this);
+  }
+
+  fetchGiphy(searchTerm) {
+    //  $.ajax({
+    //     method: 'GET',
+    //     url: `http://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&limit=5`
+    //     }).then(response => {
+    //     if (response.status !== 200){
+    //       console.log('fetchLystList. Status code: ' + response.status);
+    //       return;
+    //     }
+    //     response.json().then(data => {
+    //       this.setState({giphys: data});
+    //     });
+    //     //  this.setState({giphys: giphy});
+    //   });
+    fetch(`http://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&limit=5`)
+    .then(res => res.json())
+    .then(res => this.updateList(res));
+  }
+
+  updateList(list) {
+    this.setState({giphy: list});
+    alert('update: ' + this.state.giphy.data);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const searchTerm = this.state.searchTerm;
+    alert('searchterm:' + searchTerm);
+    this.fetchGiphy(searchTerm);
+    this.setState({ searchTerm: ""});
   }
 
   alertInput(first, last, email) {
@@ -15,7 +49,9 @@ class GiphySearchForm extends React.Component {
   }
 
   render() {
+    const giphyImage = this.state.giphy !== "" ? 'None' : this.state.giphy;
 
+    // <Text style={{color: 'black'}}>Text:{giphyImage}</Text>
     return (
       <View>
         <TextInput
@@ -36,15 +72,22 @@ class GiphySearchForm extends React.Component {
           placeholderTextColor = "#4ba37b"
           autoCapitalize = "none"
           onChangeText = {(email) => this.setState({email: email})}/>
+        <TextInput
+          style={styles.input}
+          placeholder = "SearchTerm"
+          placeholderTextColor = "#4ba37b"
+          autoCapitalize = "none"
+          onChangeText = {(searchTerm) => this.setState({searchTerm: searchTerm})}/>
         <TouchableOpacity
           style={styles.submitButton}
-          onPress = {() => this.alertInput(this.state.first, this.state.last, this.state.email)}>
+          onPress = {() => this.fetchGiphy(this.state.searchTerm)}>
           <Text style={{color: 'white', fontSize: 15}}>Submit Term</Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
+// onPress = {() => this.alertInput(this.state.first, this.state.last, this.state.email)}>
 
 const styles = StyleSheet.create({
   container: {
