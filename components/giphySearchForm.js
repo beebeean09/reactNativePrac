c401ad7dc4fe0b3e43dd5b61b4691a8654ae10cd
beebeean09 <beebeean09@gmail.com>
2017-07-07 20:32:10 -0700

    change textInput form to alert

--- components/giphySearchForm.js
+++ components/giphySearchForm.js
@@ -6,25 +6,29 @@ class GiphySearchForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { searchTerm: "", giphys: ""};
+    // this.state = { searchTerm: "", giphys: ""};
+    this.state = { firstN: "", lastN: "", email: ""};
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.alertInput = this.alertInput.bind(this);
   }
 
   fetchGiphy(searchTerm) {
-     $.ajax({
-        method: 'GET',
-        url: `http://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC`
-        }).then(response => {
-        if (response.status !== 200){
-          console.log('fetchLystList. Status code: ' + response.status);
-          return;
-        }
-        response.json().then(data => {
-          this.setState({giphys: data});
-        });
-        //  this.setState({giphys: giphy});
-      });
-
+    //  $.ajax({
+    //     method: 'GET',
+    //     url: `http://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&limit=5`
+    //     }).then(response => {
+    //     if (response.status !== 200){
+    //       console.log('fetchLystList. Status code: ' + response.status);
+    //       return;
+    //     }
+    //     response.json().then(data => {
+    //       this.setState({giphys: data});
+    //     });
+    //     //  this.setState({giphys: giphy});
+    //   });
+    var xhr = $.get(`http://api.giphy.com/v1/stickers/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&limit=5`);
+    xhr.done(data => this.setState({giphys: data}));
+    this.alertInput(searchTerm, this.state.giphys);
   }
 
   handleSubmit(e) {
@@ -39,26 +43,38 @@ class GiphySearchForm extends React.Component {
     return e => this.setState({[field]: e.target.value});
   }
 
-  alertInput(searchTerm, giphys) {
-    alert('search term: ' + searchTerm + ' giphys: ' + giphys);
+  alertInput(firstN, lastN, email) {
+    alert('firstN: ' + firstN + ' lastN: ' + lastN + ' email: ' + email);
   }
 
   render() {
     const giphyImage = this.state.giphy !== "" ? 'None' : this.state.giphy;
 
+    // <Text style={{color: 'black'}}>Text:{giphyImage}</Text>
     return (
       <View>
-        <Text style={{color: 'black'}}>Text:{giphyImage}</Text>
         <TextInput
           style={styles.input}
-          placeholder = "Search Term"
+          placeholder = "First Name"
           placeholderTextColor = "#4ba37b"
           autoCapitalize = "none"
-          onChangetext = {this.update('searchTerm')}/>
+          onChangetext = {this.update('firstN')}/>
+        <TextInput
+          style={styles.input}
+          placeholder = "Last Name"
+          placeholderTextColor = "#4ba37b"
+          autoCapitalize = "none"
+          onChangetext = {this.update('lastN')}/>
+        <TextInput
+          style={styles.input}
+          placeholder = "Email"
+          placeholderTextColor = "#4ba37b"
+          autoCapitalize = "none"
+          onChangetext = {this.update('email')}/>
         <TouchableOpacity
-          style={styles.submitbutton}
-          onPress = {() => this.alertInput(this.state.searchTerm, this.state.giphys)}>
-          <Text style={{color: '#4ba37b'}}>Submit Term</Text>
+          style={styles.submitButton}
+          onPress = {() => this.handleSubmit}>
+          <Text style={{color: 'white', fontSize: 15}}>Submit Term</Text>
         </TouchableOpacity>
       </View>
     );
@@ -73,13 +89,17 @@ const styles = StyleSheet.create({
   input: {
     margin: 20,
     height: 40,
+    padding: 10,
     borderColor: 'black',
-    borderWidth: 1
+    borderWidth: 1,
+    borderRadius: 4
   },
   submitButton: {
-    backgroundColor: '#7a42f4',
+    backgroundColor: 'grey',
+    borderColor: 'grey',
+    borderRadius: 4,
     padding: 10,
-    margin: 15,
+    margin: 20,
     height: 40,
   }
 });
